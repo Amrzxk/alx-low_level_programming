1610c790c1e0e3221a0f27449b64b841a8ebc060
Amrzxk <amrzakariya2020@yahoo.com>
2023-03-18 15:16:55 -0700

    done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Counting to 10
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
