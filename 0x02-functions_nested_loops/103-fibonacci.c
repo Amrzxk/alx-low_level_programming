cfce1a059eb206afc72f73eb2036b349931f7378
Amrzxk <amrzakariya2020@yahoo.com>
2023-03-23 06:47:40 -0700

    done

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 	unsigned long fib1 = 0, fib2 = 1, sum;
-	float total_sum = 0;
+	float total_sum;
 
 	while (1)
 	{
@@ -20,7 +20,7 @@ int main(void)
 		if (sum > 4000000)
 			break;
 
-		if ((sum % 2) == 2)
+		if ((sum % 2) == 0)
 			total_sum += sum;
 		fib1 = fib2;
 		fib2 = sum;
