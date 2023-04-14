d262d42f402b6b66c46d0a537a7e6ef70ec74e25
Amrzxk <amrzakariya2020@yahoo.com>
2023-04-14 10:49:58 -0700

    done

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,25 +1,6 @@
 #include "main.h"
 
 /**
- * _memset - function that fills memory
- *
- * @s: pointer for constant
- * @b: the constant
- * @n: maximum bytes to use
- *
- * Return: pointer s
-*/
-
-char *_memset(char *s, char b, unsigned int n)
-{
-	char *p = s;
-
-	while (n--)
-		*s++ = b;
-	return (p);
-}
-
-/**
  * _calloc - allocates memory for an array
  *
  * @nmemb: array length
@@ -35,12 +16,17 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (size == 0 || nmemb == 0)
 		return (NULL);
 
-	p = malloc(sizeof(int) * nmemb);
+	p = malloc(nmemb * size);
 
-	if (p == 0)
+	if (p == NULL)
 		return (NULL);
 
-	_memset(p, 0, sizeof(int) * nmemb);
+	char *ptr = (char *)p;
+	unsigned int i;
 
-	return (p);
+	for (i = 0; i < nmemb * size; i++)
+		*ptr++ = 0;
+
+
+	return (ptr);
 }
