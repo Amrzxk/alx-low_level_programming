c1a28695139e0a1a7395f9f00a9fa95c7a00def8
Amrzxk <amrzakariya2020@yahoo.com>
2023-04-28 03:57:48 -0700

    done

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,28 @@
 #include "lists_h"
 
 /**
+ * _strlen - returns length of string
+ * @s: the string to check
+ *
+ * Return: integer value of the length
+*/
+
+int _strlen(char *s)
+{
+        int l = 0;
+
+        if (!s)
+        {
+                return (0);
+        }
+        while (*s++)
+        {
+                l++;
+        }
+        return (l);
+}
+
+/**
  * add_node - adds a new node at the beginning of a list
  * @head: is the address of pointer to head node
  * @str: string field of node
